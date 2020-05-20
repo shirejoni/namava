d8c8a4364145b48f5947597b4d0c317bbb6fef1a
Shirejoni <hosseinshirjoni@chmail.ir>
2020-05-20 20:11:06 +0430

    Add Slider buttons and Slide logo,title

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -24,7 +24,7 @@ const fetchSlider = async (dispatch, sliderID) => {
 
 const Slider = ({sliderID}) => {
 
-    let {state, dispatch, nextSlide} = useSlider();
+    let {state, dispatch, nextSlide, previousSlide} = useSlider();
     useEffect(() => {
         fetchSlider(dispatch,sliderID);
     }, [dispatch, sliderID]);
@@ -42,8 +42,32 @@ const Slider = ({sliderID}) => {
     return (
         <div className="col-12 p-0 slider">
             {(state.succeeded && state.items.length > 0) && state.items.map((sliderItem, index) => (
-                <SliderItem key={sliderItem['id']} className={state.currentSlide == index ? 'active' : (state.previousSlide == index ? 'previous' : '')} slider={sliderItem}/>
+                <SliderItem key={sliderItem['id']} className={state.currentSlide == index ? 'active' : (state.previousSlide == index ? 'previous' : '')} slider={{
+                    ...sliderItem,
+                    title: sliderItem['caption'],
+                }}/>
             ))}
+            <div className="slider-arrowBox">
+                <div className="slider-arrow slider-arrow-left">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="#fff"
+                         className="slider-arrow--chevron" onClick={previousSlide}>
+                        <path d="M0 0h25c13.807 0 25 11.193 25 25S38.807 50 25 50H0V0z" opacity=".3"></path>
+                        <path
+                            d="M28.65 25.042c.001-.55-.218-1.078-.607-1.466l-6.906-6.906c-.812-.795-2.113-.788-2.916.016s-.8 2.104-.015 2.916l5.444 5.44-5.44 5.44c-.56.515-.795 1.296-.61 2.035s.762 1.316 1.5 1.503 1.52-.046 2.035-.606l6.906-6.906c.39-.388.607-.915.607-1.465z"
+                            opacity=".404"></path>
+                    </svg>
+                </div>
+                <div className="slider-arrow" onClick={nextSlide}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="#fff"
+                         className="slider-arrow--chevron">
+                        <path d="M0 0h25c13.807 0 25 11.193 25 25S38.807 50 25 50H0V0z" opacity=".3"></path>
+                        <path
+                            d="M28.65 25.042c.001-.55-.218-1.078-.607-1.466l-6.906-6.906c-.812-.795-2.113-.788-2.916.016s-.8 2.104-.015 2.916l5.444 5.44-5.44 5.44c-.56.515-.795 1.296-.61 2.035s.762 1.316 1.5 1.503 1.52-.046 2.035-.606l6.906-6.906c.39-.388.607-.915.607-1.465z"
+                            opacity=".404"></path>
+                    </svg>
+                </div>
+
+            </div>
         </div>
     );
 }
--- src/components/slider/SliderItem.js
+++ src/components/slider/SliderItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from 'styled-components';
 import {getNamavaUrl} from "../../utils/functions";
+import {Link} from "react-router-dom";
 
 const Slide = styled.div`
         background-image: linear-gradient(rgba(18, 18, 18, 0) 10vw, rgb(18, 18, 18) 46.875vw), linear-gradient(to left, rgba(18, 18, 18, 0.7), rgba(18, 18, 18, 0) 50%), 
@@ -15,6 +16,16 @@ const SliderItem = ({slider, className}) => {
     return (
         <div className={`slider-container ${className}`}>
             <Slide imageUrl={getNamavaUrl(slider['coverLandscape'])}>
+                <div className="slide-info-container">
+                    {slider['logoImageUrl'] && (
+                        <Link to="#">
+                            <img src={getNamavaUrl(slider['logoImageUrl'])} className="logo-image" alt={slider['title']}/>
+                        </Link>
+                    )}
+                    {slider['title'] && (
+                        <h2 className="title">{slider['title']}</h2>
+                    )}
+                </div>
             </Slide>
         </div>
     )
