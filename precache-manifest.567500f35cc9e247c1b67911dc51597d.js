9c30645677c4d6680b4a1589cf038a6953a3dd78
Hossein Shirejoni <hosseinshirjoni@chmail.ir>
2021-03-13 23:56:12 +0330

    Updates

---
+++ precache-manifest.567500f35cc9e247c1b67911dc51597d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc4ab4fc7689c75bd11fcb3a80b3753",
+    "url": "/namava/index.html"
+  },
+  {
+    "revision": "b81e75b6e73ee50e3c0b",
+    "url": "/namava/static/css/2.6f9314a4.chunk.css"
+  },
+  {
+    "revision": "8e3544078de253da1890",
+    "url": "/namava/static/css/main.9a6ba5ab.chunk.css"
+  },
+  {
+    "revision": "b81e75b6e73ee50e3c0b",
+    "url": "/namava/static/js/2.22b7ca59.chunk.js"
+  },
+  {
+    "revision": "532068105453673b0072fd406d2c8e85",
+    "url": "/namava/static/js/2.22b7ca59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3544078de253da1890",
+    "url": "/namava/static/js/main.da734799.chunk.js"
+  },
+  {
+    "revision": "6c151cc25bd5a010d781",
+    "url": "/namava/static/js/runtime-main.31199638.js"
+  }
+]);
